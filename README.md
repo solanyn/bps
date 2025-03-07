b4d1b94f92372ce2dc40311b6c8eb233384e86b9
solanyn <14799876+solanyn@users.noreply.github.com>
2025-03-07 23:04:32 +1100

    feat: use prefect

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NSW Bulk Property Sales Workflows
 
-[Flyte](https://flyte.org) workflows for collecting and processing NSW property sales data. It performs the following:
+[Prefect](https://prefect.io) workflows for collecting and processing NSW property sales data. It performs the following:
 
 - Finds all HTML `<a>` tags from the [NSW Bulk Property Sales Information](https://valuation.property.nsw.gov.au/embed/propertySalesInformation) page, which provides links to weekly and annual sales reports.
 - Downloads and stores linked files on a Minio instance (s3 compatible object storage).
---
+++ main.py
@@ -0,0 +1,60 @@
+import re
+import s3fs
+import os
+from urllib.parse import urlparse, urljoin
+from urllib.request import urlopen
+from prefect import flow, task
+from typing import List
+from bs4 import BeautifulSoup
+import httpx
+
+NSW_PROPERTY_SALES_INFORMATION_URL = (
+    "https://valuation.property.nsw.gov.au/embed/propertySalesInformation"
+)
+
+
+@task
+def get_links() -> List[str]:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    }
+
+    r = httpx.get(NSW_PROPERTY_SALES_INFORMATION_URL, headers=headers)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    zip_files = [
+        link["href"]
+        for link in soup.find_all(href=True)
+        if re.search(r"\.zip$", link["href"], re.IGNORECASE)
+    ]
+    print(zip_files)
+    return zip_files
+
+
+@task()
+def download_links(urls: List[str]) -> bool:
+    urls = [urlparse(url) for url in urls]
+    s3 = s3fs.S3FileSystem(
+        key=os.getenv("TSW_ACCESS_KEY_ID"),
+        secret=os.getenv("TSW_SECRET_ACCESS_KEY"),
+        endpoint_url=os.getenv("TSW_ENDPOINT_URL"),
+    )
+    for url in urls:
+        uri = urljoin("s3://bps", url.path)
+        if s3.exists(uri):
+            continue
+
+        with urlopen(url) as fd:
+            with s3.open(uri, "wb") as f:
+                f.write(fd.read())
+
+    return True
+
+
+@flow
+def collect() -> None:
+    urls = get_links()
+    download_links(urls)
+
+
+if __name__ == "__main__":
+    print(f"Running collect() {collect()}")
